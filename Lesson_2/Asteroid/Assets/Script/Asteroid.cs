6a234f35fc666665447da1d2e0f885c2f72c0b38
artem <cookgamer@inbox.ru>
2021-05-05 10:21:45 +0900

    Lesson_3
    Asteroid

---
+++ Lesson_2/Asteroid/Assets/Script/Asteroid.cs
@@ -0,0 +1,8 @@
+﻿namespace Asteroids
+{
+    internal sealed class Asteroid : Enemy
+    {
+
+    }
+}
+
---
+++ Lesson_2/Asteroid/Assets/Script/GameStarter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Asteroids
+{
+    internal sealed class GameStarter : MonoBehaviour
+    {
+        private void Start()
+        {
+            Enemy.CreateAsteroidEnemy(new Health(100.0f, 100.0f));
+        }
+
+    }
+}
+
